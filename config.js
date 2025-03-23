38ba6b0269aff6e324480dd2b0ba8f9a51d28eda
jakej85 <Jacobjennings85@gmail.com>
2025-03-23 11:55:57 +1100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // EMS Fencing App Configuration
 const CONFIG = {
   // Google Apps Script deployment URL
-  scriptUrl: 'https://script.google.com/macros/s/AKfycbw5zwTT4weFnZly9QIE6Jkuwmw_Rrf2ACDVVMIs-PNbCGhRbk_nfvBZRhJ8wnX20rqSag/exec',
+  scriptUrl: 'https://script.google.com/macros/s/AKfycbw5zwTT4weFnZly9QIE6Jkuwmw_Rrf2ACDVVMIs-PNbCGhRbk_nfvBZRhJ8wnX20rqSag/exec,
   
   // App version
   version: '1.0.0',
